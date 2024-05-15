eed4ae3af15989f72c13fd2f97507fa3e8abd5d3
Bruno Perry <brunoperry@gmail.com>
2024-05-15 20:57:46 +0000

    update aula10

--- aula10/script.js
+++ aula10/script.js
@@ -1,74 +1,16 @@
-const template = document.createElement('template');
-template.innerHTML = `
-    <style>
 
-        /*ELEMENTS*/
-        * {
-            font-family: system-ui, sans-serif;
-        }
-        button {
-            border: none;
-            background-color: green;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
+window.onload = () => {
+    const webGallery = document.querySelector("web-gallery");
+    webGallery.dataURL = "gallery_data.json";
 
-        /*CLASSES*/
-        .gallery {
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            width: 500px;
-            height: 500px;
-            gap: 10px;
-        }
+}
 
-        /*IDS*/
-        #images-container {
-            display: flex;
-            flex: 1;
-            background-color: red;
-        }
-        #controls {
-            display: flex;
-            justify-content: space-between;
-            background-color: blue;
-        }
-        #play-pause {
-            position: absolute;
-            top: 10px;
-            left: 10px;
-        }
-    </style>
-    <div class="gallery">
 
-        <div id="images-container"></div>
-
-        <div id="controls">
-            <button id="previous">Previous</button>
-            <button id="next">Next</button>
-        </div>
-
-        <button id="play-pause">STOP</button>
-        
-    </div>
-`;
-const itemTemplate = document.createElement('template');
-itemTemplate.innerHTML = `
-        <style>
-            .item {
-                width: 100%;
-                height: 100%;
-                background-image: url('https://imgs.search.brave.com/uvnrdXo28Rk_o7wZYO-EKCKJLxknex-iE2IL8jOhVdY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM0/MTM0MDQ3L3Bob3Rv/L3N1cmZlci5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9NHFL/bzlYeFgwbDZzTWFM/N2NNa2pJYmlFcmFP/bTJzQUJ3cWdQZXIy/ZDV6TT0');
-                background-position: center;
-                background-size: cover;
-
-            }
-        </style>
-        <div class="item"></div>
-`
+/**
 class WebGallery extends HTMLElement {
 
+    static observedAttributes = ['data-url'];
+    #galleryData;
     shadowRoot = null;
     constructor() {
         super();
@@ -95,5 +37,27 @@ class WebGallery extends HTMLElement {
             console.log('play pause clicked');
         }
     }
+
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        switch(attrName) {
+            case 'data-url':
+            this.#loadData();
+            break;
+        }
+    }
+
+    async #loadData() {
+        const req = await fetch(this.dataURL);
+        this.#galleryData = await req.json();
+        console.log(this.#galleryData)
+    }
+    
+    set dataURL(value) {
+        this.setAttribute('data-url', value);
+    }
+    get dataURL() {
+        return this.getAttribute('data-url');
+    }
 }
-customElements.define('web-gallery', WebGallery);
+*/
