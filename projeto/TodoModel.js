ef6a2927860b961c41bafe3734f88ca774225238
Bruno Perry <brunoperry@gmail.com>
2024-05-27 19:51:02 +0000

    update

--- projeto/TodoModel.js
+++ projeto/TodoModel.js
@@ -28,7 +28,8 @@ export default class TodoModel {
 
     /** TASKS */
     addTask(task) {
-        this.#tasks.push(task);
+        // this.#tasks.push(task);
+        this.#tasks.push({title: task, items:[]});
         this.#updateLocalStorage();
     }
     deleteTask(index) {
@@ -36,20 +37,26 @@ export default class TodoModel {
         this.#updateLocalStorage();
     }
     getTasks() {
+        
         return JSON.parse(localStorage.getItem("todos"));
     }
 
     /** ITEMS */
     addItem(taskIndex, item) {
-        this.#tasks[taskIndex].items.push(item);
+        // this.#tasks[taskIndex].items.push(item);
+        this.#tasks[taskIndex].items.push({title:item, checked:"false"});
         this.#updateLocalStorage();
     }
     deleteItem(taskIndex, itemIndex) {
-
         this.#tasks[taskIndex].items.splice(itemIndex, 1);
+        console.log("deletes item", this.#tasks[taskIndex].items);
         this.#updateLocalStorage();
     }
     updateItem(taskIndex, itemIndex, val) {
+
+        console.log("taskIndex", taskIndex);
+        console.log("itemIndex", itemIndex);
+        console.log(this.#tasks[taskIndex].items);
         this.#tasks[taskIndex].items[itemIndex].checked = val;
         this.#updateLocalStorage();
     }
